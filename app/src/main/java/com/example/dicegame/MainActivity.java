5700201515a558366f082ddd50488ec70ef9c0b2
Gatsdw <b.glowacki123456@gmail.com>
2024-11-04 13:25:54 +0100

    Projekt zadania 4

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -7,26 +7,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button przycisk;
-    private TextView textView;
+    final int random = new Random().nextInt(6);
+    private int count = 0;
+
+
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textRes1), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
+        TextView count = findViewById(R.id.textCount);
+
+
+        TextView res1 = findViewById(R.id.textRes1);
+        TextView res2 = findViewById(R.id.textRes2);
+        TextView res3 = findViewById(R.id.textRes1);
+        TextView res4 = findViewById(R.id.textRes2);
+        TextView res5 = findViewById(R.id.textRes2);
+
+        Button los = findViewById(R.id.buttonThrow);
+        Button restart = findViewById(R.id.buttonReset);
+
+
+        los.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                void rollDice(){
+                    res1.setText(random);
+                    res2.setText(random);
+                    res3.setText(random);
+                    res4.setText(random);
+                    res5.setText(random);
+                }
+                updateRollCount();
+
+            }
+
+
+        private void updateRollCount() {
+            count.setText("Liczba rzutów: " + count);
+
+        };
+    });
+};
 }
